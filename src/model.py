620fb8a46215eeddb6b70d528d5478c91cdd390a
Jacobo FV <priack7@hotmail.com>
2024-08-20 17:57:13 +0100

    Added spatial filtering to the model

--- src/model.py
+++ src/model.py
@@ -1,29 +1,30 @@
 import tensorflow as tf
-from keras.layers import Conv2D, Conv3D, Dense, Dropout, MaxPool3D, MaxPool2D, BatchNormalization, Activation, Concatenate, DepthwiseConv2D, AvgPool2D
+from keras.layers import Conv2D, Dense, Dropout, BatchNormalization, Activation, Concatenate, DepthwiseConv2D, Flatten
 from keras import Model, Input
 
 
-def convolution_block(inputs, nFilters: int, length: int, stride: int,  dropRate: float, name: str):
-    x = Conv2D(nFilters, (1, length), (1, stride), padding='same', name=name)(inputs)
+def convolution_block(inputs, nFilters: int, size: tuple[int, int], stride: int,  dropRate: float, name: str):
+    x = Conv2D(nFilters, size, (1, stride), name=name)(inputs)
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
     x = Dropout(dropRate)(x)
     return x
 
 
-def depth_convolution_block(inputs, length: int, stride: int, dropRate: float):
-    x = DepthwiseConv2D((1, length), (1, stride))(inputs)
+def depth_convolution_block(inputs, length: int, stride: int, dropRate: float, name: str):
+    x = DepthwiseConv2D((1, length), (1, stride), use_bias=False, name=name)(inputs)
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
     x = Dropout(dropRate)(x)
     return x
 
 
-def time_convolution_stack(inputs, length: int, nLayers: int, dropRate: float):
+def time_convolution_stack(inputs, length: int, nLayers: int, dropRate: float, name: str):
     x = inputs
     for i in range(nLayers):
-        x = depth_convolution_block(x, length, length, dropRate)
-    x = tf.reduce_mean(x, -2)
+        x = depth_convolution_block(x, length, length, dropRate, name+str(i))
+    # This only works if the final shape is [...., 1, x], otherwise use tf.reduce_mean(x, -2)
+    x = tf.squeeze(x, -2)
     return x
 
 
@@ -35,6 +36,8 @@ def ESEEGNet(shape: tuple) -> Model:
     """
     dropRate = 0.4
     nTimeFilters = 8
+    nSpaceFilters = 12
+    nFeatures = 14
     batch, d1, d2, t = shape
     # Shape [Batch, d1, d2, t]
     inputs = Input(shape=shape)
@@ -44,21 +47,32 @@ def ESEEGNet(shape: tuple) -> Model:
 
     # We apply several temporal filters to the signal with three different lengths: 0.25 s, 0.5 s, 1s. Then the
     # receptive field is increased with a concatenation of Convolutions with stride.
-    # Shape [Batch, d1 * d2, t', nTimeFilters]
-    # Shape [Batch, d1 * d2, nTimeFilters]
-    xShort = convolution_block(inputs, nTimeFilters, 16, 8, dropRate, 'ShortTimeFilters')
-    xShort = time_convolution_stack(xShort, 6, 2, dropRate)
-    xMedium = convolution_block(inputs, nTimeFilters, 32, 16, dropRate, 'MediumTimeFilters')
-    xMedium = time_convolution_stack(xMedium, 4, 2, dropRate)
-    xLong = convolution_block(inputs, nTimeFilters, 64, 32, dropRate, 'LongTimeFilters')
-    xLong = time_convolution_stack(xLong, 3, 2, dropRate)
+    # Shape (after first step) [Batch, d1 * d2, t', nTimeFilters]
+    # Shape (after second step) [Batch, d1 * d2, nTimeFilters]
+    xShort = convolution_block(inputs, nTimeFilters, (1, 16), 8, dropRate, 'ShortTimeFilters')
+    xShort = time_convolution_stack(xShort, 6, 2, dropRate, 'ShortTimeReduction')
+    xMedium = convolution_block(inputs, nTimeFilters, (1, 32), 16, dropRate, 'MediumTimeFilters')
+    xMedium = time_convolution_stack(xMedium, 4, 2, dropRate, 'MediumTimeReduction')
+    xLong = convolution_block(inputs, nTimeFilters, (1, 64), 32, dropRate, 'LongTimeFilters')
+    xLong = time_convolution_stack(xLong, 3, 2, dropRate, 'LongTimeReduction')
     # Shape [Batch, d1 * d2, nTimeFilters * 3]
     xERPMap = Concatenate(axis=-1)([xShort, xMedium, xLong])
+    # Shape [Batch, d1, d2, nTimeFilters * 3]
     xERPMap = tf.reshape(xERPMap, (batch, d1, d2, nTimeFilters * 3))
 
-    mdl = Model(inputs=inputs, outputs=xERPMap)
+    xSmall = convolution_block(xERPMap, nSpaceFilters, 3, 1, dropRate, 'SmallSizeFilters')
+    xSmall = Flatten(name='SmallFeatures')(xSmall)
+
+
+
+    # Shape [Batch, features]
+    # feats = Concatenate(axis=-1)([xSmall, xMid, xLarge])
+    hidden = Dense(nFeatures, activation='relu')(xSmall)
+    output = Dense(2, activation='softmax')(hidden)
+
+    mdl = Model(inputs=inputs, outputs=output)
     mdl.summary()
+    return mdl
 
 shape = (100, 8, 7, 321)
 mdl = ESEEGNet(shape)
-
